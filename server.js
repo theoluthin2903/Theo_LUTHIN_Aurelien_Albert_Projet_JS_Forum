7109c29525e78d2092d9bca0a8d7eadf590fe54c
ltheo <theo.luthin@ynov.com>
2026-06-17 18:58:43 +0200

    modif server nom de fichier incorrect

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/api/health', (req, res) => {
 
 // Serve frontend
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'authentification.html'));
 });
 
 app.get('/forum', (req, res) => {
